77c1cc629b8883977eb62ff821d5082724301c62
Arya Gupta <artagupta239gmail.com>
2025-01-26 22:33:00 +0530

    first task done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
+
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -21,7 +21,6 @@ function App() {
          <h1>Welcome To Paradise Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
-         
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
